475f1da02f645585c6d7e07e0b34c30e1b13c81b
Trilussa <rickynullo@gmail.com>
2025-08-16 00:44:25 +0200

    wecercer

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const pool = mysql.createPool({
     user: "mio-db",
     password: "Riccardo2003",
     database: "mio-db_events",
-        port: 5000,
+        port: "5000",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
