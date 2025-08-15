51ccf6918fda5b544088bd6927c75031659bdc55
Trilussa <rickynullo@gmail.com>
2025-08-15 23:31:43 +0200

    3306

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 3306;
 
 app.use(express.json({ limit: '16mb' }));
 app.use(cookieParser());
